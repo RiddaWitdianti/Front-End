3195c326fcc1fe5fc11269cc1ca04dadf1682185
Ridda Witdianti <witdiantiridda@gmail.com>
2023-02-14 19:45:57 +0700

    project front-end

---
+++ logictest/test1.js
@@ -0,0 +1,19 @@
+let words = "beever";
+
+// Buatlah skema logika untuk memuat kata diatas menjadi berbentuk seperti berikut :
+// b
+// be
+// bee
+// beev
+// beeve
+// beever
+
+import React, { Component } from 'react'
+
+export default class test1 extends Component {
+  render() {
+    return (
+      <div>test1</div>
+    )
+  }
+}
